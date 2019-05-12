7249bc44e327544672d59947c8d6f5e7bf320783
AhmedIelsayed <a7mad.i.elsayed10@gmail.com>
2019-05-12 22:50:30 +0200

    blo2

--- src/Game.java
+++ src/Game.java
@@ -6,6 +6,7 @@ public class Game {
         System.out.println("nazelo intellij lol");
         System.out.println("hii");
         System.out.println("Test");
+        System.out.println("blo2");
 
     }
 }
