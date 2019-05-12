eccfadedc5634cb62d03489edc2134f7fdcb64b5
haniaraslan <48217810+haniaraslan@users.noreply.github.com>
2019-05-13 01:13:28 +0200

    Update FatalBomb.java

--- src/FatalBomb.java
+++ src/FatalBomb.java
@@ -1,2 +1,18 @@
 public class FatalBomb extends Bombs {
+	private boolean bomb = false;
+
+	public int changes(int lives) {
+		return isSliced() ? 0 : lives;
+	}
+
+	@Override
+	public boolean isSliced() {
+		return bomb; // if true lives == 0 else nothing
+	}
+
+	@Override
+	public void slice() {
+		bomb = true;
+	}
+
 }
