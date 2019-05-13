2dd3195e5dfdc3bd94a4775b57a18b2030dea503
AhmedIelsayed <a7mad.i.elsayed10@gmail.com>
2019-05-13 02:52:49 +0200

    7aga malhash ay lazma

--- src/NormalLevel.java
+++ src/NormalLevel.java
@@ -1,2 +1,16 @@
-public class NormalLevel extends Level {
+public class NormalLevel implements Level {
+    @Override
+    public int getLevelSpeed() {
+        return 2;
+    }
+
+    @Override
+    public int getNoOfBombs() {
+        return 2;
+    }
+
+    @Override
+    public int getNoOfFruits() {
+        return 2;
+    }
 }
