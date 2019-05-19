3a185489693052b56b12c38c0e74200658741b83
haniaraslan <48217810+haniaraslan@users.noreply.github.com>
2019-05-19 22:07:12 +0200

    Update Bombs.java

--- src/Bombs.java
+++ src/Bombs.java
@@ -2,87 +2,85 @@ import javafx.scene.image.Image;
 import javafx.scene.shape.Path;
 
 public abstract class Bombs implements GameObject {
-    private int Xlocation;
-    private int Ylocation;
-    private int maxHeight;
-    private int initialVelocity;
-    private int fallingVelocity;
-    private Boolean isSliced;
-    private Boolean hasMovedOffScreen;
-    private Image[] image;
-    private Path rdmPath;
+	private int Xlocation;
+	private int Ylocation;
+	private int maxHeight;
+	private int initialVelocity;
+	private int fallingVelocity;
+	private Boolean isSliced;
+	private Boolean hasMovedOffScreen;
+	private Image[] image;
+	private Path rdmPath;
 
+	public Bombs(Image[] image, Path path) {
+		Xlocation = 0;
+		Ylocation = 0;
+		maxHeight = 0;
+		initialVelocity = 0;
+		fallingVelocity = 0;
+		isSliced = false;
+		hasMovedOffScreen = false;
+		this.image = image;
+		rdmPath = path;
+	}
 
-    public Bombs(Image[] image,Path path){
-        Xlocation=0;
-        Ylocation=0;
-        maxHeight=0;
-        initialVelocity=0;
-        fallingVelocity=0;
-        isSliced=false;
-        hasMovedOffScreen=false;
-        this.image =image;
-        rdmPath=path;
-    }
+	@Override
+	public int getXlocation() {
+		return Xlocation;
+	}
 
-    @Override
-    public int getXlocation() {
-        return Xlocation;
-    }
+	@Override
+	public int getYlocation() {
+		return Ylocation;
+	}
 
-    @Override
-    public int getYlocation() {
-        return Ylocation;
-    }
+	@Override
+	public int getMaxHeight() {
+		return maxHeight;
+	}
 
-    @Override
-    public int getMaxHeight() {
-        return maxHeight;
-    }
+	@Override
+	public int getInitialVelocity() {
+		return initialVelocity;
+	}
 
-    @Override
-    public int getInitialVelocity() {
-        return initialVelocity;
-    }
+	@Override
+	public int getSliceScore() {
+		return 0;
+	}
 
-    @Override
-    public int getSliceScore() {
-        return 0;
-    }
+	@Override
+	public int getFallingVelocity() {
+		return fallingVelocity;
+	}
 
-    @Override
-    public int getFallingVelocity() {
-        return fallingVelocity;
-    }
+	@Override
+	public boolean isSliced() {
+		return isSliced;
+	}
 
-    @Override
-    public boolean isSliced() {
-        return isSliced;
-    }
+	@Override
+	public boolean hasMovedOffScreen() {
+		return hasMovedOffScreen;
+	}
 
-    @Override
-    public boolean hasMovedOffScreen() {
-        return hasMovedOffScreen;
-    }
+	@Override
+	public void slice() {
+		isSliced = true;
+	}
 
-    @Override
-    public void slice() {
-    isSliced=true;
-    }
+	@Override
+	public void move(double time) {
 
-    @Override
-    public void move(double time) {
+	}
 
-    }
+	@Override
+	public Image[] getImage() {
+		return image;
+	}
 
-    @Override
-    public Image[] getImage() {
-        return image;
-    }
+	public Path getPath() {
+		return rdmPath;
+	}
 
-    public Path getPath() {
-        return rdmPath;
-    }
-
-   
 }
