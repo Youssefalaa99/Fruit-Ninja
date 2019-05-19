868de79c94089b1f8fbe0e459b3b7054fa801129
haniaraslan <48217810+haniaraslan@users.noreply.github.com>
2019-05-19 22:30:53 +0200

    Update NormalLevel.java

--- src/NormalLevel.java
+++ src/NormalLevel.java
@@ -1,16 +1,16 @@
 public class NormalLevel implements Level {
-    @Override
-    public double getLevelSpeed() {
-        return 1.2;
-    }
+	@Override
+	public double getLevelSpeed() {
+		return 1.2;
+	}
 
-    @Override
-    public double getNoOfBombs() {
-        return 1.2;
-    }
+	@Override
+	public double getNoOfBombs() {
+		return 1.2;
+	}
 
-    @Override
-    public double getNoOfFruits() {
-        return 1.2;
-    }
+	@Override
+	public double getNoOfFruits() {
+		return 1.2;
+	}
 }
