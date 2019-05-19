229949a96e5cb221dae1698032c8639329d36dad
haniaraslan <48217810+haniaraslan@users.noreply.github.com>
2019-05-19 22:31:34 +0200

    Update DangerousBomb.java

--- src/DangerousBomb.java
+++ src/DangerousBomb.java
@@ -3,14 +3,13 @@ import javafx.scene.shape.Path;
 
 public class DangerousBomb extends Bombs {
 
-	public DangerousBomb(Image[] image, Path path){
-		super(image,path);
+	public DangerousBomb(Image[] image, Path path) {
+		super(image, path);
 	}
 
 	public int changes(int lives) {
 		return isSliced() ? --lives : lives;
 	}
-
 
 	@Override
 	public void slice() {
