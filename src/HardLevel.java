b85c0743ee548815abdec197b0cd32361ed88f6f
haniaraslan <48217810+haniaraslan@users.noreply.github.com>
2019-05-19 22:30:14 +0200

    Update HardLevel.java

--- src/HardLevel.java
+++ src/HardLevel.java
@@ -1,16 +1,16 @@
 public class HardLevel implements Level {
-    @Override
-    public double getLevelSpeed() {
-        return 1.5;
-    }
+	@Override
+	public double getLevelSpeed() {
+		return 1.5;
+	}
 
-    @Override
-    public double getNoOfBombs() {
-        return 1.5;
-    }
+	@Override
+	public double getNoOfBombs() {
+		return 1.5;
+	}
 
-    @Override
-    public double getNoOfFruits() {
-        return 1.5;
-    }
+	@Override
+	public double getNoOfFruits() {
+		return 1.5;
+	}
 }
