fe263bd2fac6630746b863e6f05e056edef25c46
haniaraslan <48217810+haniaraslan@users.noreply.github.com>
2019-05-19 22:36:11 +0200

    Update EasyLevel.java

--- src/EasyLevel.java
+++ src/EasyLevel.java
@@ -1,16 +1,16 @@
 public class EasyLevel implements Level {
-    @Override
-    public double getLevelSpeed() {
-        return 1;
-    }
+	@Override
+	public double getLevelSpeed() {
+		return 1;
+	}
 
-    @Override
-    public double getNoOfBombs() {
-        return 1;
-    }
+	@Override
+	public double getNoOfBombs() {
+		return 1;
+	}
 
-    @Override
-    public double getNoOfFruits() {
-        return 1;
-    }
+	@Override
+	public double getNoOfFruits() {
+		return 1;
+	}
 }
